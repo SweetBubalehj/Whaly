b42f82e1bf29832be120df2832d1913719d99aef
Bubaleh <mlg.mario.420@gmail.com>
2023-11-17 22:50:09 +0300

    fixes and comments

--- whaly-contracts/contracts/WhalyFactory.sol
+++ whaly-contracts/contracts/WhalyFactory.sol
@@ -6,15 +6,15 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./WhalyThread.sol";
 
-interface IERC {
-    function balanceOf(address owner) external view returns (uint256);
-
-    function name() external view returns (string memory);
-
-    function symbol() external view returns (string memory);
+interface IWhalyThread {
+    function getCommentsCount() external view returns (uint);
 }
 
-contract WlahyFactory is ERC721, ReentrancyGuard, Ownable {
+/**
+ * @title WhalyFactory
+ * @dev A contract for creating and managing Whaly threads associated with ERC721 tokens.
+ */
+contract WhalyFactory is ERC721, ReentrancyGuard, Ownable {
     mapping(address => bool) public isTokenThreaded;
     mapping(address => uint) public nftTokenIdToThreadToken;
     mapping(address => address) public tokenToThreadAddress;
@@ -26,10 +26,19 @@ contract WlahyFactory is ERC721, ReentrancyGuard, Ownable {
 
     event MintThread(address indexed token, uint256 indexed threadId);
 
+    /**
+     * @dev Initializes the WhalyFactory contract.
+     * @param _ipfsURI The base URI for IPFS metadata.
+     */
     constructor(string memory _ipfsURI) ERC721("Whaly", "WHALY") {
         ipfsBaseURI = _ipfsURI;
     }
 
+    /**
+     * @dev Checks if the given address is an ERC721 token.
+     * @param _token The address of the token to check.
+     * @return A boolean indicating whether the address is an ERC721 token.
+     */
     function isERC(address _token) public view returns (bool) {
         try IERC(_token).name() returns (string memory) {
             return true;
@@ -46,10 +55,20 @@ contract WlahyFactory is ERC721, ReentrancyGuard, Ownable {
         return false;
     }
 
+    /**
+     * @dev Checks if the given string is empty.
+     * @param _string The string to check.
+     * @return A boolean indicating whether the string is empty.
+     */
     function isEmptyString(string memory _string) internal pure returns (bool) {
         return bytes(_string).length == 0;
     }
 
+    /**
+     * @dev Mints a new WhalyThread associated with an ERC721 token.
+     * @param _token The address of the ERC721 token.
+     * @param comment The comment for the thread.
+     */
     function mintThread(
         address _token,
         string memory comment
@@ -76,14 +95,17 @@ contract WlahyFactory is ERC721, ReentrancyGuard, Ownable {
     }
 
     /**
-     * @dev See {ERC721-_baseURI}.
+     * @dev Returns the base URI for IPFS metadata.
+     * @return The base URI for IPFS metadata.
      */
     function _baseURI() internal view override returns (string memory) {
         return ipfsBaseURI;
     }
 
     /**
-     * @dev See {IERC721Metadata-tokenURI}.
+     * @dev Returns the token URI for a given token ID.
+     * @param tokenId The ID of the token.
+     * @return The token URI.
      */
     function tokenURI(
         uint256 tokenId
@@ -93,10 +115,37 @@ contract WlahyFactory is ERC721, ReentrancyGuard, Ownable {
     }
 
     /**
-     * @dev Function for changing base URI value.
-     * @param _newBaseURI new base URI value
+     * @dev Changes the base URI for IPFS metadata.
+     * @param _newBaseURI The new base URI.
      */
     function changeBaseURI(string memory _newBaseURI) public onlyOwner {
         ipfsBaseURI = _newBaseURI;
+    }
+
+    /**
+     * @dev Returns the count of thread tokens.
+     * @return The count of thread tokens.
+     */
+    function getThreadTokenCount() public view returns (uint) {
+        return threadTokens.length;
+    }
+
+    /**
+     * @dev Returns an array of comment counts for each thread address.
+     * @return An array of comment counts.
+     */
+    function getThreadAddressesCommentCounts()
+        public
+        view
+        returns (uint[] memory)
+    {
+        uint[] memory commentCounts = new uint[](threadAddresses.length);
+
+        for (uint i = 0; i < threadAddresses.length; i++) {
+            commentCounts[i] = IWhalyThread(threadAddresses[i])
+                .getCommentsCount();
+        }
+
+        return commentCounts;
     }
 }
--- whaly-contracts/contracts/WhalyThread.sol
+++ whaly-contracts/contracts/WhalyThread.sol
@@ -5,6 +5,10 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 interface IERC {
     function balanceOf(address owner) external view returns (uint256);
+
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
 }
 
 contract WhalyThread is ReentrancyGuard {
@@ -20,6 +24,12 @@ contract WhalyThread is ReentrancyGuard {
     address public token;
     uint256 public whalyTokenId;
 
+    /**
+     * @dev Constructor function that initializes the contract.
+     * @param _token The address of the token contract.
+     * @param _whalyTokenId The ID of the Whaly token.
+     * @param _comment The initial comment for the sender.
+     */
     constructor(address _token, uint256 _whalyTokenId, string memory _comment) {
         token = _token;
         whalyTokenId = _whalyTokenId;
@@ -28,6 +38,10 @@ contract WhalyThread is ReentrancyGuard {
         commentedAddresses.push(msg.sender);
     }
 
+    /**
+     * @dev Adds a comment to the contract.
+     * @param _comment The content of the comment.
+     */
     function addComment(string memory _comment) public nonReentrant {
         require(!isEmptyString(_comment), "Empty comment");
         require(
@@ -39,6 +53,9 @@ contract WhalyThread is ReentrancyGuard {
         commentedAddresses.push(msg.sender);
     }
 
+    /**
+     * @dev Removes the comment of the sender.
+     */
     function removeComment() public nonReentrant {
         require(
             !isEmptyString(addressToComment[msg.sender].content),
@@ -49,6 +66,10 @@ contract WhalyThread is ReentrancyGuard {
         delete addressToComment[msg.sender];
     }
 
+    /**
+     * @dev Likes a comment.
+     * @param _commentOwner The address of the comment owner.
+     */
     function likeComment(address _commentOwner) public nonReentrant {
         require(
             !addressToComment[_commentOwner].likedBy[msg.sender],
@@ -59,6 +80,10 @@ contract WhalyThread is ReentrancyGuard {
         addressToComment[_commentOwner].likedBy[msg.sender] = true;
     }
 
+    /**
+     * @dev Removes a like from a comment.
+     * @param _commentOwner The address of the comment owner.
+     */
     function removeCommentLike(address _commentOwner) public nonReentrant {
         require(
             addressToComment[_commentOwner].likedBy[msg.sender],
@@ -69,6 +94,10 @@ contract WhalyThread is ReentrancyGuard {
         addressToComment[_commentOwner].likedBy[msg.sender] = false;
     }
 
+    /**
+     * @dev Removes an address from the commentedAddresses array.
+     * @param _userAddress The address to be removed.
+     */
     function removeAddressFromArray(address _userAddress) internal {
         uint indexToRemove;
         for (uint i = 0; i < commentedAddresses.length; i++) {
@@ -97,14 +126,27 @@ contract WhalyThread is ReentrancyGuard {
         commentedAddresses = newAddresses;
     }
 
+    /**
+     * @dev Checks if a string is empty.
+     * @param _string The string to check.
+     * @return A boolean indicating whether the string is empty or not.
+     */
     function isEmptyString(string memory _string) internal pure returns (bool) {
         return bytes(_string).length == 0;
     }
 
+    /**
+     * @dev Gets the number of comments in the contract.
+     * @return The number of comments.
+     */
     function getCommentsCount() public view returns (uint) {
         return commentedAddresses.length;
     }
 
+    /**
+     * @dev Gets the balances of the addresses that have commented.
+     * @return An array of balances.
+     */
     function getAddressesBalance() public view returns (uint[] memory) {
         uint[] memory balances = new uint[](commentedAddresses.length);
 
